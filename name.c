e6faddc72a2c0cd65e6ecc2818d14268bab24d19
B0923016 <pitt20020608@gmail.com>
2022-06-30 21:20:46 +0800

    上傳

---
+++ name.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+// for(;;) while(1) for(;;) 寫的時候先打好語法，再去填判斷
+
+int main() {
+	int a = 10;
+
+	if (a < 20) {
+		printf("a is less than 20\n");
+	} else {
+		printf("a is not less than 20\n");
+	}
+
+	return 0; // Code Style Please 一定要注意 請寫
+}
